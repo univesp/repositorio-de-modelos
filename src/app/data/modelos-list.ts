cc13d7abe8db54d09805eb008eb51e32f614dac1
Gabriel Henrique Gallo da Silva <gabrielsilva@fpanet.inet>
2025-02-12 13:48:27 -0300

    atualizando lista de modelos

--- src/app/data/modelos-list.ts
+++ src/app/data/modelos-list.ts
@@ -4,80 +4,154 @@ export const Modeloslist = [
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg1.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md1.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg1.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Dolor Sit"
+    autor: "Dolor Sit",
+    destaque: false
   },
   {
     titulo: "Modelo AD Dolor",
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg2.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md2.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg2.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Lorem Ipsum"
+    autor: "Lorem Ipsum",
+    destaque: false
   },
   {
     titulo: "Modelo CD Sit",
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg3.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md3.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg3.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Amet Sit"
+    autor: "Amet Sit",
+    destaque: false
   },
   {
     titulo: "Modelo AD Amet",
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg4.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md1.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg4.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Sit Lorem"
+    autor: "Sit Lorem",
+    destaque: false
   },
   {
     titulo: "Modelo PG Ipsum",
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg5.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md2.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg5.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Lorem Sit"
+    autor: "Lorem Sit",
+    destaque: false
   },
   {
     titulo: "Modelo PE Labore",
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg6.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md3.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg6.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Sit Ipsum"
+    autor: "Sit Ipsum",
+    destaque: false
   },
   {
     titulo: "Modelo EC Elit",
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg7.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md1.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg7.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Ipsum Amet"
+    autor: "Ipsum Amet",
+    destaque: false
   },
   {
     titulo: "Modelo EP Sed",
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg8.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md2.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg8.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Amet Lorem"
+    autor: "Amet Lorem",
+    destaque: false
   },
   {
     titulo: "Modelo MA Amet",
     date: "Out 22, 2024",
     disciplina: "EDU400",
     categorias: ["Jogo", "Interativo", "RA", "RV"],
-    imagem: "https://assets.univesp.br/repositorio-de-modelos/card-lg9.png",
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md3.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg9.png",
     descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
-    autor: "Lorem Dolor"
+    autor: "Lorem Dolor",
+    destaque: false
+  },
+
+  {
+    titulo: "Modelo EC Elit",
+    date: "Out 22, 2024",
+    disciplina: "EDU400",
+    categorias: ["Jogo", "Interativo", "RA", "RV"],
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md1.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg1.png",
+    descricao: "Maecenas feugiat auctor rhoncus. Proin imperdiet imperdiet ante, non rhoncus orci ultricies quis. Donec id tincidunt libero, at facilisis magna. Quisque non ultricies ante, eu suscipit lorem. Nullam sagittis aliquam tellus, in varius felis...",
+    autor: "Ipsum Amet",
+    destaque: true
+  },
+  {
+    titulo: "Modelo EP Sed",
+    date: "Out 22, 2024",
+    disciplina: "EDU400",
+    categorias: ["Jogo", "Interativo", "RA", "RV"],
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md2.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg2.png",
+    descricao: "Aliquam eu sodales lorem. In lacus ante, eleifend varius imperdiet nec, facilisis id nisi. Donec consectetur congue aliquet. Mauris at elit tellus. Mauris iaculis diam lacus, et ultrices tellus efficitur in. Etiam id sapien ut diam rhoncus euismod...",
+    autor: "Amet Lorem",
+    destaque: true
+  },
+  {
+    titulo: "Modelo MA Amet",
+    date: "Out 22, 2024",
+    disciplina: "EDU400",
+    categorias: ["Jogo", "Interativo", "RA", "RV"],
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md3.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg3.png",
+    descricao: "Curabitur ornare magna arcu, eu vehicula nibh tincidunt eu. Duis auctor tortor at ornare consectetur. Vestibulum mi dolor, feugiat eget euismod id, euismod et orci. Proin consequat, est eu congue mattis...",
+    autor: "Lorem Dolor",
+    destaque: true
+  },
+  {
+    titulo: "Modelo EC Elit",
+    date: "Out 22, 2024",
+    disciplina: "EDU400",
+    categorias: ["Jogo", "Interativo", "RA", "RV"],
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md1.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg4.png",
+    descricao: "Maecenas feugiat auctor rhoncus. Proin imperdiet imperdiet ante, non rhoncus orci ultricies quis. Donec id tincidunt libero, at facilisis magna. Quisque non ultricies ante, eu suscipit lorem. Nullam sagittis aliquam tellus, in varius felis...",
+    autor: "Ipsum Amet",
+    destaque: true
+  },
+  {
+    titulo: "Modelo EP Sed",
+    date: "Out 22, 2024",
+    disciplina: "EDU400",
+    categorias: ["Jogo", "Interativo", "RA", "RV"],
+    img_md: "https://assets.univesp.br/repositorio-de-modelos/img-md2.png",
+    img_lg: "https://assets.univesp.br/repositorio-de-modelos/card-lg5.png",
+    descricao: "Aliquam eu sodales lorem. In lacus ante, eleifend varius imperdiet nec, facilisis id nisi. Donec consectetur congue aliquet. Mauris at elit tellus. Mauris iaculis diam lacus, et ultrices tellus efficitur in. Etiam id sapien ut diam rhoncus euismod...",
+    autor: "Amet Lorem",
+    destaque: true
   }
 ];
